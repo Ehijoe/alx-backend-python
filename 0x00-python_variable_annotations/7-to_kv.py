d9c08c7dd18d45a7b3340b0785247dc798350906
Joseph Alikah <alikahjoseph@gmail.com>
2023-04-28 05:26:12 +0100

    0x00 - Fix tuples and lists

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Complex types - string and int/float to tuple."""
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a string and a number squared."""
     return (k, float(v) ** 2)
