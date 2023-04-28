46e56a31de2b5d17591ecf1672b40056a36ca1e5
Joseph Alikah <alikahjoseph@gmail.com>
2023-04-28 05:49:33 +0100

    0x00 - Fix task 101

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Any."""
-from typing import Any, Sequence
+from typing import Any, Sequence, Union
 
 
 # The types of the elements of the input are not know
-def safe_first_element(lst: Sequence[Any]) -> Any:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Return the first element of a list."""
     if lst:
         return lst[0]
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """TypeVar."""
 from typing import TypeVar, Union, Any, Mapping
 
