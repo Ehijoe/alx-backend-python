fe3c456c5e638e447423b98d350eaed5f49f4b45
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-04 01:47:46 +0100

    0x02 - Fix task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Coroutine to generate numbers."""
 from random import uniform
-from time import sleep
+from asyncio import sleep
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
     """Yield 10 random numbers asynchronously."""
     for i in range(10):
-        sleep(1)
+        await sleep(1.0)
         yield uniform(0, 10)
