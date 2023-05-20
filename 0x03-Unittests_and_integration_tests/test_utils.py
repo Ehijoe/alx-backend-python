5952cc47ad49da72f1bb6113fbc8c13fc2d4d3e8
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-20 23:18:59 +0100

    0x03 - Add tests for access_nested_map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Tests for the utils.py file."""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests for accessing nested maps."""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, result):
+        """Test accessing nested maps."""
+        self.assertEqual(access_nested_map(nested_map, path), result)
