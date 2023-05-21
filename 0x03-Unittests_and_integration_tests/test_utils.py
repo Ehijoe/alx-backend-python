07fcc81ad91d0a8728ed406839841f1ee7c984e8
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-21 12:56:35 +0100

    0x03 - Add tests for memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -40,3 +40,24 @@ class TestGetJson(unittest.TestCase):
             pat.return_value = Mock()
             pat.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test the memoize function."""
+
+    def test_memoize(self):
+        """Test the memoize function."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as test_method:
+            test_method.return_value = 42
+            test = TestClass()
+            self.assertEqual(test.a_property, 42)
+            self.assertEqual(test.a_property, 42)
+            test_method.assert_called_once()
