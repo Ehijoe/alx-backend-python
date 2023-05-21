e87e254f41b6f4e77d9369092f77352008d7101a
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-21 23:31:00 +0100

    0x03 - Add tests for github client public repos property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,7 +32,29 @@ class TestGithubOrgClient(TestCase):
         fake_org = {
             "repos_url": repos,
         }
-        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as fake:
+        with patch.object(GithubOrgClient, "org",
+                          new_callable=PropertyMock) as fake:
             fake.return_value = fake_org
             self.assertEqual(github_client._public_repos_url,
                              fake_org["repos_url"])
+
+    @patch(
+        "client.get_json",
+        return_value=[
+            {
+                "name": "Test Repo",
+                "license": {"key": "test"},
+            },
+        ]
+    )
+    def test_public_repos(self, get_json):
+        """Test the public_repos property."""
+        with patch.object(GithubOrgClient,
+                          "_public_repos_url",
+                          new_callable=PropertyMock) as public_repos_url:
+            public_repos_url.return_value = "https://some.random.url"
+            github_client = GithubOrgClient("google")
+            self.assertEqual(github_client.public_repos(),
+                             ["Test Repo"])
+            public_repos_url.assert_called_once()
+            get_json.assert_called_once()
