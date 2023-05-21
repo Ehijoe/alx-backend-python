39b53b6bb1f91e75817bbad506910755e2a331a0
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-21 12:36:52 +0100

    0x03 - Add tests for get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Tests for the utils.py file."""
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,3 +25,18 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, exception):
         """Test exceptions for access_nested_map."""
         self.assertRaises(exception, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests for the get_json function."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test the get_json function."""
+        with patch("requests.get") as pat:
+            pat.return_value = Mock()
+            pat.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
