14d602c8c34ba67ea1035b538d490603f0e22a7f
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-21 23:04:03 +0100

    0x03 - Add tests for github client public repos property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Tests for client.py."""
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -20,3 +20,19 @@ class TestGithubOrgClient(TestCase):
             self.assertEqual(github_client.org, "Response")
             self.assertEqual(github_client.org, "Response")
             get.assert_called_once_with(github_client.ORG_URL.format(org=org))
+
+    @parameterized.expand([
+        (["http://A", "http://B", "http://C"],),
+        (["http://qwdq.wq", "http://adfsf"],),
+        ([],),
+    ])
+    def test_public_repos_url(self, repos):
+        """Test the public_repos_url property"""
+        github_client = GithubOrgClient("google")
+        fake_org = {
+            "repos_url": repos,
+        }
+        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as fake:
+            fake.return_value = fake_org
+            self.assertEqual(github_client._public_repos_url,
+                             fake_org["repos_url"])
