65924d9be04f41003096547bf517291b4f98cde6
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-22 23:47:44 +0100

    0x03 - Add integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Tests for client.py."""
 from unittest import TestCase
-from unittest.mock import patch, PropertyMock
-from parameterized import parameterized
+from unittest.mock import patch, PropertyMock, Mock
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(TestCase):
@@ -66,3 +67,42 @@ class TestGithubOrgClient(TestCase):
     def test_has_license(self, repo, key, contained):
         """Test the has_license method."""
         self.assertEqual(GithubOrgClient.has_license(repo, key), contained)
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(TestCase):
+    """Integration tests for the Github client."""
+
+    @classmethod
+    def setUpClass(self):
+        """Setup a mocking of requests.get."""
+        def requester(url):
+            """Return the test data for a particular url."""
+            response = Mock()
+            if url == self.org_payload.get("repos_url"):
+                response.json.return_value = self.repos_payload
+            else:
+                response.json.return_value = self.org_payload
+            return response
+
+        patcher = patch("requests.get", side_effect=requester)
+        self.requester = patcher.start()
+
+    def test_public_repos(self):
+        """Test that the correct public repos are returned."""
+        github_client = GithubOrgClient("google")
+        self.assertEqual(github_client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """Test that the correct public repos are returned."""
+        github_client = GithubOrgClient("google")
+        self.assertEqual(github_client.public_repos(license="apache-2.0"),
+                         self.apache2_repos)
+
+    @classmethod
+    def tearDownClass(self):
+        """Stop the mock of requests.get."""
+        self.requester.stop()
